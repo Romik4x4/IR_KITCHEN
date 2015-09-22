764c83fa86ed323e52f030ba7c1b1cf053f0639a
Roma Kuzmin <romik-off-road@yandex.ru>
2015-09-22 14:02:43 +0300

    Ports

--- IR_KITCHEN.ino
+++ IR_KITCHEN.ino
@@ -8,7 +8,24 @@
 
 #include <IRremote.h>
 
-int RECV_PIN = 2;
+#define DEBUG 0
+
+#define K1  16724175
+#define K2  16718055
+#define K3  16743045
+#define K4  16716015
+
+#define Z1  16582903
+#define Z2  16615543
+#define Z3  16599223
+#define Z4  16591063
+
+#define  RECV_PIN 3
+
+#define PORT1  4
+#define PORT2  5
+#define PORT3  6
+#define PORT4  7
 
 IRrecv irrecv(RECV_PIN);
 
@@ -16,14 +33,39 @@ decode_results results;
 
 void setup()
 {
-  Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
+  pinMode(A0,OUTPUT);
+  
+  pinMode(PORT1,OUTPUT); digitalWrite(PORT1,LOW);
+  pinMode(PORT2,OUTPUT); digitalWrite(PORT2,LOW);
+  pinMode(PORT3,OUTPUT); digitalWrite(PORT3,LOW);
+  pinMode(PORT4,OUTPUT); digitalWrite(PORT4,LOW);
+  
+  digitalWrite(A0,HIGH);
+  
+  if (DEBUG) Serial.begin(9600);
+
+  irrecv.enableIRIn(); 
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    irrecv.resume(); // Receive the next value
+
+  if (irrecv.decode(&results)) {  
+   
+    if (DEBUG) Serial.println(results.value);
+   
+   switch(results.value) {     
+    case Z1: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH); break;
+    case Z2: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case Z3: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case Z4: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case K1: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case K2: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case K3: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;
+    case K4: digitalWrite(A0,LOW);  delay(200); digitalWrite(A0,HIGH);break;      
+   }
+    
+    irrecv.resume();
   }
-  delay(100);
+  
 }
+
